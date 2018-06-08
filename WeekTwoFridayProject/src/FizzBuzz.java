b218b74a53aea310cd9dcde4aa2d556a4820046e
Melissa Fung <rainekiss@gmail.com>
2018-06-07 21:26:58 -0400

    This won't take long version of FizzBuzz

--- WeekTwoFridayProject/src/FizzBuzz.java
+++ WeekTwoFridayProject/src/FizzBuzz.java
@@ -8,7 +8,16 @@ public class FizzBuzz {
     protected static void printNumbers() {
 
         String word = "";
-        int firstNum=0, lastNum=0;
+        int firstNum=0, lastNum=0, firstLastDiff=0;
+
+        firstNum = getRandomNumber(1,100);
+        lastNum = getRandomNumber(1,100);
+
+        firstLastDiff = Math.abs(firstNum-lastNum);
+
+        if(firstLastDiff < 10) {
+            System.out.println("This won't take long");
+        }
 
         for(int i=1; i<=100; i++){
             word = "";
@@ -26,11 +35,11 @@ public class FizzBuzz {
             else{
 
                 if(i == 1){
-                    firstNum = getRandomNumber(1,100);
+
                     System.out.println(firstNum);
                 }
                 else if(i == 100){
-                    lastNum = getRandomNumber(1,100);
+
                     System.out.println(lastNum);
                 }
                 else {
