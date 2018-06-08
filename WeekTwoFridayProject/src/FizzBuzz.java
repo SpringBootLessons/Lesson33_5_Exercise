670f818b6bdc7fe611f2f2cefcd066c1bbf28ba7
Melissa Fung <rainekiss@gmail.com>
2018-06-07 21:20:12 -0400

    Shazam version of FizzBuzz

--- WeekTwoFridayProject/src/FizzBuzz.java
+++ WeekTwoFridayProject/src/FizzBuzz.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class FizzBuzz {
     public static void main(String[] args){
@@ -7,6 +8,7 @@ public class FizzBuzz {
     protected static void printNumbers() {
 
         String word = "";
+        int firstNum=0, lastNum=0;
 
         for(int i=1; i<=100; i++){
             word = "";
@@ -18,13 +20,38 @@ public class FizzBuzz {
                 word += "Buzz";
             }
 
-            if(!word.equals("")){
+            if(!word.equals("") && i != 1 && i != 100){
                 System.out.println(word);
             }
             else{
-                System.out.println(i);
+
+                if(i == 1){
+                    firstNum = getRandomNumber(1,100);
+                    System.out.println(firstNum);
+                }
+                else if(i == 100){
+                    lastNum = getRandomNumber(1,100);
+                    System.out.println(lastNum);
+                }
+                else {
+                    System.out.println(i);
+                }
+            }
+
+            if(firstNum == lastNum){
+                System.out.println("Shazam!!");
             }
 
         }
+    }
+
+    protected static int getRandomNumber(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
